c5900f59150830ca9a907eb273d5864c5fe9112a
Serg8787 <tsybulnikserg@gmail.com>
2021-06-25 12:38:07 +0300

    find textview

--- app/src/main/java/com/example/homework88/MainActivity.java
+++ app/src/main/java/com/example/homework88/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.homework88;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+   private TextView tx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tx = findViewById(R.id.tvTextview);
+
     }
 }
